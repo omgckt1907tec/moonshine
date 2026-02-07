76704285b059525295119470d85ff7c9ef7149f8
omgckt1907tec <omgckt1907@gmail.com>
2026-02-07 21:14:14 +0530

    Update config.js

--- moonshinebox/config.js
+++ moonshinebox/config.js
@@ -3,17 +3,17 @@ const CONFIG = {
 
     background: "background.jpg",
 
-    min1: "min1.jfif",
+    min1: "min1.jpg",
     min2: "min2.jpg",
     min3: "max3.jpg",
-    min4: "max4.webp",
+    min4: "max4.jpg",
     min5: "min5.jpg",
     min6: "min6.jpg",
 
     max1: "max1.jpg",
     max2: "max2.jpg",
     max3: "max3.jpg",
-    max4: "max4.webp",
-    max5: "max5.png",
+    max4: "max4.jpg",
+    max5: "max5.jpg",
     max6: "max6.jpg", 
 }
